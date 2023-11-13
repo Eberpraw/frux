f8c64457cfa35362aa428d158551b9f8c8a7c10d
Eberpraw <toby@easy.poker>
2023-11-13 20:12:42 +0100

    Added comma seperated search for our test product

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
+# from flask import Flask
 
 # Function to get product details from the user
-def get_product_details(existing_prices):
-    product_name = input("Enter product name: ")
-
+def get_product_details(existing_prices, product_name):
     # Check if the product already has a price in the CSV file
     if product_name in existing_prices:
         price_dkk = existing_prices[product_name]
@@ -17,21 +16,21 @@ def get_product_details(existing_prices):
         'Price (DKK)': price_dkk,
     }
 
+# Replace 'ee786ff7897e4963ea57b1dc0f2bd73724bf9190' with the actual commit hash or branch name
 github_csv_url = 'https://raw.githubusercontent.com/Eberpraw/frux/ee786ff7897e4963ea57b1dc0f2bd73724bf9190/database.csv'
 
 # Read existing prices from the CSV file
 existing_prices_df = pd.read_csv(github_csv_url)
 existing_prices = dict(zip(existing_prices_df['Product Name'], existing_prices_df['Price (DKK)']))
 
-# Get user input for multiple products
+# Get user input for multiple products (comma-separated)
+products_input = input("Enter product names (comma-separated): ")
+selected_product_names = [name.strip() for name in products_input.split(',')]
+
+# Get product details for each selected product
 products_data = []
-
-while True:
-    add_another = input("Do you want to add another product? (yes/no): ").lower()
-    if add_another != 'yes':
-        break
-
-    product_details = get_product_details(existing_prices)
+for product_name in selected_product_names:
+    product_details = get_product_details(existing_prices, product_name)
     if product_details['Price (DKK)'] is not None:
         products_data.append(product_details)
 
