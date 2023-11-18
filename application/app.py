490ab41a3571a3f3a52c15c467aca0f5eb22507e
Eberpraw <toby@easy.poker>
2023-11-18 20:22:36 +0100

    Fixed issue with comma separated text search and styles some css

--- application/app.py
+++ application/app.py
@@ -12,11 +12,16 @@ def index():
 @app.route("/items", methods=["GET", "POST"])
 def items():
     if request.method == "POST":
-        item = request.form.get("item")
-        if not item:
+        item_input = request.form.get("item")
+        if not item_input:
             return abort(400, "No item provided")
-        # Process the item as needed
-        chosenItems.append(item)
+        
+        # Process the input items as needed
+        items = [item.strip() for item in item_input.split(',')]
+        
+        # Add each item to chosenItems
+        chosenItems.extend(items)
+        
         return redirect("/grocery-list")
     else:
         return render_template("items.html")
@@ -26,13 +31,14 @@ def grocery_list():
     # Call the function to get prices based on chosen items
     supermarket_prices = get_product_details_rema1000(chosenItems)
 
-    # split up items to a list (seperated by comma) 
+    # split up items to a list (separated by comma) 
     result = [item.strip() for sublist in chosenItems for item in sublist.split(',')]
-    
+
     # Render the template with the prices
     return render_template("grocery-list.html", supermarket_prices=supermarket_prices, chosenItems=result)
 
 
+# Get the products from Rema 1000 in database.csv
 def get_product_details_rema1000(chosen_items, supermarket='Rema 1000', csv_url='https://raw.githubusercontent.com/Eberpraw/frux/5b6fcd790d597b263028317fce51a1db34af7dc5/database.csv'):
     df = pd.read_csv(csv_url)
 
@@ -43,6 +49,5 @@ def get_product_details_rema1000(chosen_items, supermarket='Rema 1000', csv_url=
     prices = dict(zip(filtered_df['Product Name'], filtered_df['Price (DKK)']))
 
     print("Supermarket Prices:", prices)
-
 
     return prices
